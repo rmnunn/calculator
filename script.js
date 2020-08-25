be4d5a2ccae5c84e745a0f626fbf8d12ed6dac82
Reece Nunn <rmnunn5@gmail.com>
2020-08-25 14:51:05 -0700

    First commit

---
+++ script.js
@@ -0,0 +1,36 @@
+const calculator = document.querySelector('.calculator')
+const keys = calculator.querySelector('.calculator__keys')
+const display = document.querySelector('.calculator__display')
+
+keys.addEventListener('click', e => {
+    if (e.target.matches('button')) {
+        const key = e.target
+        const action = key.dataset.action
+        
+        if (!action) {
+            console.log('number key!')
+          }
+
+        if (
+            action === 'add' ||
+            action === 'subtract' ||
+            action === 'multiply' ||
+            action === 'divide'
+            ) {
+            console.log('operator key!')
+            key.classList.add('is-depressed')
+            }
+
+        if (action === 'decimal') {
+            console.log('decimal key!')
+            }
+              
+        if (action === 'clear') {
+            console.log('clear key!')
+            }
+              
+        if (action === 'calculate') {
+            console.log('equal key!')
+            }
+    }
+   })
